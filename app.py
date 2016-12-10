86eb00e8b791d90ea7808ef781890577a09fbed0
Rahul Patil <loginrahul90@gmail.com>
2016-12-10 19:35:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ app = Flask(__name__)
 def home():
    return "TEST 123 Home Page"
 
+@app.route('/demo')
+def demo():
+    return "DEMO PAGE"
+
+
 if __name__ == '__main__':
    app.run(host="0.0.0.0",debug=True)
