81741475bc36f74dad326f43e26147362c62545d
Rahul Patil <loginrahul90@gmail.com>
2016-12-10 14:08:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from flask import Flask
 
 
@@ -6,3 +8,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
    return "Home Page"
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0",debug=True)
